b09fe8eb553ae4a8ec7b7cf6da14a2e2dfa7cffb
Aaran McGuire <aaran.mcguire@cl.vc>
2017-03-21 23:47:57 +0000

    Add Select and Copy by clicking.

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,32 @@ var app = new Vue({
 				passphraseWords.push(this.dictionary[Math.floor(Math.random() * this.dictionary.length)]);
 			}
 			this.passphrase = passphraseWords.join(this.passphraseSettings.seperator);
+		},
+		selectAndCopyPassphrase: function (el) {
+			var sel, range;
+			if (window.getSelection && document.createRange) { //Browser compatibility
+				sel = window.getSelection();
+				if (sel.toString() == '') { //no text selection
+					window.setTimeout(function(){
+						range = document.createRange(); //range object
+						range.selectNodeContents(el); //sets Range
+						sel.removeAllRanges(); //remove all ranges from selection
+						sel.addRange(range);//add Range to a Selection.
+						this.copySelectedText();
+					},1);
+				}
+			} else if (document.selection) { //older ie
+				sel = document.selection.createRange();
+				if (sel.text == '') { //no text selection
+					range = document.body.createTextRange();//Creates TextRange object
+					range.moveToElementText(el);//sets Range
+					range.select(); //make selection.
+					this.copySelectedText();
+				}
+			}
+		},
+		copySelectedText: function () {
+			var copied = document.execCommand('copy');
 		}
 	}
 });
